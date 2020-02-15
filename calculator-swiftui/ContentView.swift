afbca14c21aae92f7262ceb324cbdb0d88ac6d55
Cameron Buckley <cbuckley@astate.edu>
2020-02-15 10:59:13 -0600

    Init

--- calculator-swiftui/ContentView.swift
+++ calculator-swiftui/ContentView.swift
@@ -9,11 +9,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var fieldOne : String = "0"
+    @State private var fieldTwo : String = "0"
+    @State private var answer : Double = 0.0
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Group{
+                Text("My Calculator")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.blue)
+                Text("by me")
+                Spacer()
+                    .frame(height: 100.0)
+                TextField("First Value", text: $fieldOne)
+                    .padding(.leading, 100.0)
+                TextField("Second Value", text: $fieldTwo)
+                    .padding(.leading, 100.0)
+            }
+            Group{
+                HStack(spacing: 27.0) {
+                    Button(action: {self.addition()}) {
+                        Text("+")
+                    }
+                    Button(action: {}) {
+                        Text("-")
+                    }
+                    Button(action: {}) {
+                        Text("x")
+                    }
+                }
+                
+
+            }
+            Text("\(answer)")
+                .padding(.top, 19.0)
+            Spacer()
+                .frame(height: 450.0)
+//end of groups
+        }
+    }
+
+    func addition(){
+        let num1 = Double(fieldOne)
+        let num2 = Double(fieldTwo)
+        answer = num1! + num2!
+        
+        //print("tapped")
     }
 }
-
+//end of contentView
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
